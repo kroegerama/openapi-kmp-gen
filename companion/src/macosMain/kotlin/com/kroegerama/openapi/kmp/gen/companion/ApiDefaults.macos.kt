394ac60e6a65808e2562625ef4b72fcb5b9cc03d
kroegerama <1519044+kroegerama@users.noreply.github.com>
2025-10-06 17:09:27 +0200

    improve macos `platformUserAgent`

--- companion/src/macosMain/kotlin/com/kroegerama/openapi/kmp/gen/companion/ApiDefaults.macos.kt
+++ companion/src/macosMain/kotlin/com/kroegerama/openapi/kmp/gen/companion/ApiDefaults.macos.kt
@@ -3,5 +3,5 @@ package com.kroegerama.openapi.kmp.gen.companion
 import platform.Foundation.NSProcessInfo
 
 internal actual val platformUserAgent: String = NSProcessInfo.processInfo.run {
-    "Darwin ${operatingSystemName()}/$operatingSystemVersionString"
+    "Darwin macOS/$operatingSystemVersionString"
 }
