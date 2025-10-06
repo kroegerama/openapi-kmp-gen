c674ec6e6162eac5b267496efa6e0478938ecdb7
kroegerama <1519044+kroegerama@users.noreply.github.com>
2025-10-06 16:46:04 +0200

    cleanup

--- generated/src/commonMain/kotlin/main/HttpClientExperiment.kt
+++ generated/src/commonMain/kotlin/main/HttpClientExperiment.kt
@@ -3,10 +3,7 @@ package main
 import com.kroegerama.kmp.gen.generated.Api
 import com.kroegerama.kmp.gen.generated.Auth
 import com.kroegerama.kmp.gen.generated.api.DefaultApi
-import com.kroegerama.kmp.gen.generated.models.IntegerTest
 import com.kroegerama.openapi.kmp.gen.companion.AuthItem
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.plugin
 import io.ktor.client.request.forms.FormDataContent
 import io.ktor.client.request.forms.MultiPartFormDataContent
 import io.ktor.client.request.forms.formData
