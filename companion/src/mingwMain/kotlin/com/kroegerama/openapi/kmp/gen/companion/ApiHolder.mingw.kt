094a929a23fe65d5d34712f6d253fe95810ec60d
kroegerama <1519044+kroegerama@users.noreply.github.com>
2025-10-07 10:27:18 +0200

    improve `platformUserAgent` for `mingw` target

--- companion/src/mingwMain/kotlin/com/kroegerama/openapi/kmp/gen/companion/ApiHolder.mingw.kt
+++ companion/src/mingwMain/kotlin/com/kroegerama/openapi/kmp/gen/companion/ApiHolder.mingw.kt
@@ -3,20 +3,35 @@ package com.kroegerama.openapi.kmp.gen.companion
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.winhttp.WinHttp
+import kotlinx.cinterop.CFunction
+import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.alloc
+import kotlinx.cinterop.invoke
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
+import kotlinx.cinterop.reinterpret
 import kotlinx.cinterop.sizeOf
-import platform.windows.GetVersionExW
-import platform.windows.OSVERSIONINFOW
+import platform.windows.GetModuleHandleA
+import platform.windows.GetProcAddress
+import platform.windows.OSVERSIONINFOEXW
 
 @OptIn(ExperimentalForeignApi::class)
 public actual val platformUserAgent: String = memScoped {
-    val versionInfo = alloc<OSVERSIONINFOW>()
-    versionInfo.dwOSVersionInfoSize = sizeOf<OSVERSIONINFOW>().toUInt()
-    GetVersionExW(versionInfo.ptr)
-    "WinHTTP Windows/${versionInfo.dwMajorVersion}.${versionInfo.dwMinorVersion}.${versionInfo.dwBuildNumber}"
+    val osvi = alloc<OSVERSIONINFOEXW>()
+    osvi.dwOSVersionInfoSize = sizeOf<OSVERSIONINFOEXW>().toUInt()
+
+    val module = GetModuleHandleA("ntdll.dll") ?: return@memScoped "WinHTTP"
+    val address = GetProcAddress(module, "RtlGetVersion") ?: return@memScoped "WinHTTP"
+
+    val rtlGetVersion = address.reinterpret<CFunction<(CPointer<OSVERSIONINFOEXW>?) -> Int>>()
+
+    val status = rtlGetVersion(osvi.ptr)
+    if (status == 0) {
+        "WinHTTP Windows/${osvi.dwMajorVersion}.${osvi.dwMinorVersion}.${osvi.dwBuildNumber}"
+    } else {
+        "WinHTTP"
+    }
 }
 
 internal actual fun createBaseClient(block: HttpClientConfig<*>.() -> Unit): HttpClient {
