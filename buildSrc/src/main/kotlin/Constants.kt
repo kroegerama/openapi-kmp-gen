7600a7aeb37974178d680b07c7be9c0bd5fa45d4
kroegerama <1519044+kroegerama@users.noreply.github.com>
2025-11-19 22:41:44 +0100

    increase version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,9 +1,9 @@
 object C {
 
     const val PROJECT_NAME = "OpenAPI KMP Gen"
-    const val PROJECT_DESCRIPTION = "Generate modern API Clients in Kotlin from OpenAPI specifications. Supports OpenAPI >= 3.0.0."
+    const val PROJECT_DESCRIPTION = "Generate modern API Clients in Kotlin multiplatform from OpenAPI specifications. Supports OpenAPI >= 3.0.0."
 
-    const val PROJECT_VERSION = "1.0.0-beta01"
+    const val PROJECT_VERSION = "1.0.0-rc01"
     const val PROJECT_GROUP_ID = "com.kroegerama.openapi-kmp-gen"
     const val PROJECT_WEBSITE = "https://github.com/kroegerama/openapi-kmp-gen"
     const val PROJECT_GIT = "https://github.com/kroegerama/openapi-kmp-gen.git"
