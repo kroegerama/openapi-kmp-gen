f97a1eb5a606e2059d0e5852a6ea5717bf978329
kroegerama <1519044+kroegerama@users.noreply.github.com>
2026-09-10 18:36:26 +0200

    cleanup optimization rules for r8

--- companion/build.gradle.kts
+++ companion/build.gradle.kts
@@ -54,11 +54,6 @@ kotlin {
             jvmTarget = JvmTarget.JVM_11
         }
 
-        optimization {
-            consumerKeepRules.publish = true
-            consumerKeepRules.file("consumer-proguard-rules.pro")
-        }
-
         withHostTest { }
     }
 
